c5ab856a5ba33dd1daefa9c5eaea2080cde4f132
Valentin Panov <valentin.panov@me.com>
2024-02-27 14:54:27 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/ssr",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=30, stale-while-revalidate=59",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
